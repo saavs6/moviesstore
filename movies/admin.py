c37983bff423881a3582c14e6738344135ca6012
saavs6 <sseguin2006@gmail.com>
2025-09-07 20:08:23 -0400

    v1.0

---
+++ movies/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Movie, Review
+class MovieAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    search_fields = ['name']
+admin.site.register(Movie,MovieAdmin)
